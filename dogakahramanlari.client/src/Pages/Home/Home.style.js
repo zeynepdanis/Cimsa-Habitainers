c4e8c4b4d8484e6757d3b9c10bc71c04575425aa
Aysu Nur Terzi <80470813+aysunurterzi@users.noreply.github.com>
2024-02-27 13:18:10 +0300

    Home page düzenlemeleri
    header ekledim,
    butonları koydum,

--- dogakahramanlari.client/src/Pages/Home/Home.style.js
+++ dogakahramanlari.client/src/Pages/Home/Home.style.js
@@ -1,5 +1,50 @@
 import styled from '@emotion/styled';
 
-export const HomeWrapper = styled.div`
 
+export const HomeWrapper = styled.div`
+    .container {
+        padding: 20px 30px;
+        border-radius: 10px;
+        position: absolute;
+        top: 40%;
+        left: 50%;
+        transform: translate(-50% , -50%);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 15px;
+        }
+
+        .header{
+            padding: 20px 30px;
+        border-radius: 10px;
+        position: absolute;
+        top: 20%;
+        left: 50%;
+        transform: translate(-50% , -50%);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 15px;
+
+            font-size: 50px;
+            color: black;
+        }
+
+        .button-container {
+            display: flex;
+            justify-content: flex-start;
+            width: 100%;
+            gap: 40px;
+            .back-icon {
+                background-color: transparent;
+                padding: 0;
+            }
+            .button {
+                background-color: #A1C298;
+                padding: 15px;
+                font-weight: bold;
+            }
+        }
+    }
 `;
