46b9edc6581a71b00624276350e8f63c409fdce2
Efsa Tortuk <okyanus09efsa@gmail.com>
2024-02-28 16:16:33 +0300

    register oluşturuldu

--- dogakahramanlari.client/src/App.jsx
+++ dogakahramanlari.client/src/App.jsx
@@ -1,19 +1,21 @@
-import Login from './Pages/Login/Login';
-import Home from './Pages/Home/Home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.css';
+import Login from "./Pages/Login/Login";
+import Home from "./Pages/Home/Home";
+import Register from "./Pages/Register/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/login' element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route index element={<Home />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
