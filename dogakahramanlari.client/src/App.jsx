0ea6f0d30c3c0601bd5260b2e42b5de2c69b530a
Aysu Nur Terzi <80470813+aysunurterzi@users.noreply.github.com>
2024-03-08 01:42:57 +0300

    ForestPage
    ForestPage dosyası eklendi

--- dogakahramanlari.client/src/App.jsx
+++ dogakahramanlari.client/src/App.jsx
@@ -1,21 +1,23 @@
-import Login from "./Pages/Login/Login";
-import Home from "./Pages/Home/Home";
-import Register from "./Pages/Register/Register";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
-import Dashboard from "./Pages/Dashboard/Dashboard";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AnimalsInCages from './Pages/AnimalsInCages/AnimalsInCages';
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import Forest from "./Pages/ForestPage/Forest";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
+import "./index.css";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />} />
           <Route path='/cages' element={<AnimalsInCages />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/forest" element={<Forest />} />
           <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} /> 
         </Routes>
       </BrowserRouter>
     </div>
---
+++ dogakahramanlari.client/src/Pages/ForestPage/Forest.jsx
@@ -0,0 +1,14 @@
+import { Wrapper } from './Forest.style';
+
+const Forest = () => {
+    return (
+        <Wrapper>
+            <h1>aysuuu</h1>
+
+        </Wrapper>
+
+
+    )
+}
+
+export default Forest;
