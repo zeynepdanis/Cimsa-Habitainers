7593f44d9ae6ade38c66d6b877d80dcdb8ca6bd7
Damla Yilmaz <damlanur.ylmaz@hotmail.com>
2024-03-16 16:39:50 +0300

    login request

--- dogakahramanlari.client/src/Pages/Login/Login.jsx
+++ dogakahramanlari.client/src/Pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Background from '../Background/Background';
 import { LoginWrapper } from './Login.style';
 import { BackIcon, Password, UserIcon } from '../../assets/svg/icons';
@@ -6,10 +6,37 @@ import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const navigate = useNavigate();
+    const [loginData, setLoginData] = useState({
+        username: '',
+        password: ''
+    })
+
+    const usernameHandler = (e) => {
+        const usernameData = e.target.value;
+        setLoginData({...loginData , username: usernameData});
+    };
+
+    const passwordHandler = (e) => {
+        const passwordData = e.target.value;
+        setLoginData({...loginData , password: passwordData});
+    };
     
     const backHomeHandler = () => {
         const newPath = '/';
         navigate(newPath);
+    };
+
+    const loginHandler = async () => {
+            const loginRequest = await fetch('http://localhost:5120/api/authentication/login', {
+              method: 'POST',
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({username: loginData.username, password: loginData.password})
+            });
+            const content = await loginRequest.json();
+            console.log(content)
     };
 
   return (
@@ -23,17 +50,26 @@ const Login = () => {
                 <div className='input-part'>
                     <div className='input-area'>
                         <UserIcon />
-                        <input className='input' placeholder='Kullanıcı Adı'/>
+                        <input 
+                            className='input' 
+                            placeholder='Kullanıcı Adı'
+                            onChange={(e) => usernameHandler(e)}
+                        />
                     </div>
                     <div className='input-area'>
                         <Password />
-                        <input type='password' className='input' placeholder='Şifre'/>
+                        <input 
+                            type='password' 
+                            className='input' 
+                            placeholder='Şifre'
+                            onChange={(e) => passwordHandler(e)}
+                        />
                     </div>
                 </div>
             </div>
             <div className='button-container'>
                 <button onClick={backHomeHandler} className='back-icon'> <BackIcon /> </button>
-                <button className='button'>Girişi Tamamla</button>
+                <button onClick={loginHandler} className='button'>Girişi Tamamla</button>
             </div>
         </div>
     </LoginWrapper>
