c4c689c36b68e8a5fc8fa54365b215bd66cb5c48
Aysu Nur Terzi <80470813+aysunurterzi@users.noreply.github.com>
2024-03-16 17:21:58 +0300

    Home page finnished
    home page bitti

--- dogakahramanlari.client/src/Pages/Home/Home.jsx
+++ dogakahramanlari.client/src/Pages/Home/Home.jsx
@@ -1,6 +1,24 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import "./HomeStyle.css";
 
+
+
 const Home = () => {
+
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate('/login'); // 'login' rotasına yönlendirme yapar
+  };
+
+  const handleRegisterClick = () => {
+    navigate('/register'); // 'register' rotasına yönlendirme yapar
+  };
+
+
+
+
   return (
 
     <div className='home-wrapper'>
@@ -11,13 +29,13 @@ const Home = () => {
 
       <div className='home-container'>
         <div className='home-button-container'>
-          <button className='home-button'>
+          <button className='home-button' onClick={handleLoginClick}>
             <div className="home-text-container">
               <p className='home-text'>GİRİŞ YAP</p>
             </div>
             <img className='board-img' src="/icon/board.png" alt="board-icon" />
           </button>
-          <button className='home-button'>
+          <button className='home-button' onClick={handleRegisterClick}>
             <div className="home-text-container">
               <p className='home-text'>KAYIT OL</p>
             </div>
