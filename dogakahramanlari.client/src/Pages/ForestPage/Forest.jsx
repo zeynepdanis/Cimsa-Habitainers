06a53fc18332a37e2edc3f4ee6ddbbe31502423d
Aysu Nur Terzi <80470813+aysunurterzi@users.noreply.github.com>
2024-03-16 17:59:42 +0300

    anasayfa button
    forest page e anasayfa butonu ekledim icon şeklinde

--- dogakahramanlari.client/src/Pages/ForestPage/Forest.jsx
+++ dogakahramanlari.client/src/Pages/ForestPage/Forest.jsx
@@ -1,8 +1,18 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import "./ForestStyle.css";
+
+
 
 const Forest = () => {
     const [animals, setAnimals] = useState([]);
+
+    const navigate = useNavigate();
+
+    const handleDashboardClick = () => {
+        navigate('/dashboard'); // 'login' rotasına yönlendirme yapar
+    };
+
 
     useEffect(() => {
         fetch("http://localhost:5120/api/animals")
@@ -19,27 +29,34 @@ const Forest = () => {
     }, []);
 
     return (
-        <div className="forest-wrapper">
-            <div className="forest-header-container">
-                <div className="forest-header">
-                    <h1>ORMAN</h1>
-                </div>
-                <div className="forest-header2">
-                    <h1>ORMAN</h1>
-                </div>
+
+        <>
+            <div className="homepage" onClick={handleDashboardClick}>
+                <img className="dashboard-image" src="icon/dashboard.png" alt="mainpage" />
             </div>
-            <div id="forest-animal-container">
-                {animals.map(animal => (
-                    <div className={`animal-${animal.id}`} key={animal.id}>
-                        <img
-                            className={`animal-${animal.id}`}
-                            src={animal.status === 1 ? animal.imagesStatus1 : animal.imagesNormal}
-                            alt={animal.name}
-                        />
+
+            <div className="forest-wrapper">
+                <div className="forest-header-container">
+                    <div className="forest-header">
+                        <h1>ORMAN</h1>
                     </div>
-                ))}
+                    <div className="forest-header2">
+                        <h1>ORMAN</h1>
+                    </div>
+                </div>
+                <div id="forest-animal-container">
+                    {animals.map(animal => (
+                        <div className={`animal-${animal.id}`} key={animal.id}>
+                            <img
+                                className={`animal-${animal.id}`}
+                                src={animal.status === 1 ? animal.imagesStatus1 : animal.imagesNormal}
+                                alt={animal.name}
+                            />
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
