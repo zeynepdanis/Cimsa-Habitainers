4347fd9e729180ef811ac963ffe8dbf3d986dc1d
Damla Yilmaz <damlanur.ylmaz@hotmail.com>
2024-03-17 13:05:45 +0300

    dashboard duzenlendi

--- dogakahramanlari.client/src/Pages/Dashboard/Dashboard.jsx
+++ dogakahramanlari.client/src/Pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { Animals } from "../../assets/svg/icons"
+import { AnimalIcon, HomeIcon } from "../../assets/svg/icons"
 import { DashboardWrapper } from "./Dashboard.style"
 
 const Dashboard = () => {
@@ -8,18 +8,25 @@ const Dashboard = () => {
   return (
     <DashboardWrapper>
         <div className="dashboard">
+            <div className="dashboard-header">
+                    {/* <div><HomeIcon /></div> */}
+                    <h1>DOĞA KAHRAMANLARI</h1>
+            </div>
             <div className="dashboard-container">
                 <p>
                 Hoş Geldiniz! 
                 <br />
                 Hayvanları Kurtarın!
                 Ormanda yaşayan dostlarımız, bir gün ormanlarını
-                tehdit eden büyük bir sorunla karşılaştılar. İşte tam da bu noktada, cesur çocuklar ortaya çıktı ve hayvanların yardımına koştu.
-                Şimdi siz de bir kahraman olabilirsiniz! Biz, hayvanları korumanın ve onlara sevgiyle yardım etmenin önemini anlatan bir web sitesiyiz. Haydi, birlikte hayvanların kurtuluşu için adım atalım!
-                </p>
-                <div className="animal-container">
-                    <Animals />
+                tehdit eden büyük bir sorunla karşılaştılar. Çevre kirliliği onları evlerinden ayırıyor. Bu şekilde giderse nesilleri tükenecek. 
+                İşte tam da bu noktada, siz cesur çocuklar ortaya çıktınız ve hayvanların yardımına koştunuz.
+                Şimdi siz de bir kahraman olabilirsiniz! 
+                 Haydi, birlikte hayvanların kurtuluşu için adım atalım!
+                 Onların evlerini, doğamızı koruyalım!
+                 <div className="animal-container">
+                    <AnimalIcon />
                 </div>
+                </p>
             </div>
             <div className="button-container">
                 <button 
--- dogakahramanlari.client/src/Pages/Dashboard/Dashboard.style.js
+++ dogakahramanlari.client/src/Pages/Dashboard/Dashboard.style.js
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 
 export const DashboardWrapper = styled.div`
     background-image: url("../../assets/jpgs/forest_background_transparency.jpg");
+    padding: 40px;
     .dashboard {
         position: absolute;
         top: 50%;
@@ -11,19 +12,36 @@ export const DashboardWrapper = styled.div`
         flex-direction: column;
         gap: 40px;
         margin: 30px;
+        .dashboard-header {
+            position: absolute;
+            top: 30px;
+            left: 50%;
+            transform: translate(-55% , -150px);
+            white-space: nowrap;
+            font-family: "Seymour One", sans-serif;
+            font-style: normal;
+            font-size: 20px;
+            line-height: normal;
+            text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+            color: rgba(48, 92, 4, 0.94);
+            display: flex;
+            align-items: center;
+        }
         .dashboard-container {
-            background: rgba(183, 238, 129, 0.82);
+            background: rgba(160, 174, 242, 0.56);
             box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
             padding: 20px 40px;
             border-radius: 10px;
-            color: rgba(48, 92, 4, 0.94);
+            color: rgba(48, 2, 8, 0.94);
             font-style: italic;
             font-weight: bold;
-            font-size: 24px;
+            font-size: 28px;
             width: 1000px;
+            margin-top: 30px;
+            position: relative;
             .animal-container {
-                display: flex;
-                flex-direction: flex-end;
+                position: absolute;
+                right: -100px;
             }
         }
         .button-container {
@@ -34,8 +52,8 @@ export const DashboardWrapper = styled.div`
             .dashboard-button {
                 border-radius: 60%;
                 padding: 40px;
-                background-color: rgba(183, 238, 129, 0.94);
-                font-weight: 400;
+                background-color: #7F5539;
+                font-weight: bold;
                 font-size: 26px;
                 font-style: italic;
             }
