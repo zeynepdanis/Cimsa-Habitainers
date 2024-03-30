9cedfc26c9aff7167add6556ebd717e020c26cb5
Beyza Cebeci <bcebecii@outlook.com>
2024-03-31 00:18:52 +0300

    güncel dev çekildi, eksik backend kodları eklendi

--- DogaKahramanlari.Server/Services/Contracts/IUserKeyService.cs
+++ DogaKahramanlari.Server/Services/Contracts/IUserKeyService.cs
@@ -6,9 +6,9 @@ namespace DogaKahramanlari.Server.Services.Contracts
     public interface IUserKeyService
     {
         Task<IEnumerable<UserKey>> GetAllUserKeysAsync(bool trackChanges);
-        Task<UserKey> GetOneUserKeyByIdAsync(int id, bool trackChanges);
+        Task<UserKey> GetOneUserKeyByIdAsync(string id, bool trackChanges);
         Task<UserKeyDto> CreateOneUserKeyAsync(UserKeyDtoForInsertion userKeyDtoForInsertion);
-        Task UpdateOneUserKeyAsync(int id, UserKeyDtoForUpdate userkeyDto, bool trackChanges);
+        Task UpdateOneUserKeyAsync(string id, UserKeyDtoForUpdate userkeyDto, bool trackChanges);
 
 
     }
